6eb41569b31be93d87aa293f7aad1c04a77146de
1407268 <1407268@EEE1407268>
2018-12-17 22:46:05 +0530

    doubt reg. about me page

---
+++ index.js
@@ -0,0 +1,11 @@
+AOS.init({
+    duration:1000,  
+});
+
+jQuery(document).ready(function(){
+	jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},2000);
+	});
+});
---
+++ js/index.js
@@ -0,0 +1,28 @@
+jQuery(document).ready(function(){
+	jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},2000);
+	});
+   
+    
+    
+    
+    function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+  }
+  // If element is scrolled into view, fade it in
+  $(window).scroll(function() {
+    $('.imgpara').each(function() {
+      if (isScrolledIntoView(this) === true) {
+        $(this).addClass('fadeInLeft');
+      }
+    });
+  });
+});
